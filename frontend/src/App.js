f997c13187113a64714fdb86dc4347b2556b7df9
Mile Alexander <milealexander@gmail.com>
2023-01-31 11:41:18 +0100

    Moved the content to Main.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
+import React from "react";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <>
+      <Header />
+      <Main />
+    </>
   );
-}
+};
 
 export default App;
